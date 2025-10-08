ee5daf0b3b215bf67e8ce0aa3c1fe8fc5c402f69
phucle04 <157397067+phucle04@users.noreply.github.com>
2025-10-08 19:54:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const btn = document.getElementById("btn");
+const title = document.getElementById("title");
+
+btn.addEventListener("click", () => {
+    title.textContent = "Bạn vừa bấm nút!";
+    title.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+});
 
